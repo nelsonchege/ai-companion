51e2bbf0e38e110e740a7e692309fec8c1f5f83b
nelson chege <nelson3chege@gmail.com>
2023-08-29 03:11:01 +0300

    update: display companions

--- app/(root)/(routes)/page.tsx
+++ app/(root)/(routes)/page.tsx
@@ -1,15 +1,41 @@
 import Categories from "@/components/Categories";
+import CompanionList from "@/components/CompanionList";
 import SearchInput from "@/components/SearchInput";
 import prisma from "@/lib/db";
 
-export default async function Home() {
+type HomeProps = {
+  searchParams: {
+    categoryId: string;
+    name: string;
+  };
+};
+export default async function Home({ searchParams }: HomeProps) {
+  const companions = await prisma.companion.findMany({
+    where: {
+      categoryId: searchParams.categoryId,
+      name: {
+        search: searchParams.name,
+      },
+    },
+    orderBy: {
+      createdAt: "desc",
+    },
+    include: {
+      _count: {
+        select: {
+          messages: true,
+        },
+      },
+    },
+  });
+
   const categories = await prisma.category.findMany();
-  console.log("categories---------------->", categories);
   return (
     <div>
       <div className="h-full p-4 space-y-2">
         <SearchInput />
         <Categories categories={categories} />
+        <CompanionList companions={companions} />
       </div>
     </div>
   );
